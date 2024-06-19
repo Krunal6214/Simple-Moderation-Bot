6e9dca30ec8db62d18e022b76646d67713b5616f
KAP6214 <kap621400@gmail.com>
2024-06-19 23:02:32 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    token: process.env.TOKEN, 
+    status: 'Never Gonna Give you Up',
+    modlogchannel: '1253020395980980287',
+    warnlog: '1253020395980980287',
+    welcomechannel: '1253020395980980287',
+}
