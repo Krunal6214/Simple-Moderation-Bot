780e9baf0d1ce44c792227d83e0a04957a3c4665
KAP6214 <kap621400@gmail.com>
2024-06-19 23:04:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     token: process.env.TOKEN, 
     status: 'Never Gonna Give you Up',
-    modlogchannel: '1253020395980980287',
-    warnlog: '1253020395980980287',
-    welcomechannel: '1253020395980980287',
+    modlogchannel: '',
+    warnlog: '',
+    welcomechannel: '',
 }
